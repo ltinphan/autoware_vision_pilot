0ffcd4aa0c5eb30d31fc13ff80231a375d91be22
Atanasko Boris Mitrev <atanasko.mitrev@exo.mk>
2026-04-08 14:09:02 +0200

    AutoSteer 2.0 add code for data conversion, model architecture, training and visualization

--- Models/inference/auto_steer_infer.py
+++ Models/inference/auto_steer_infer.py
@@ -10,82 +10,45 @@ from torchvision import transforms
 import numpy as np
 
 sys.path.append('..')
-from Models.model_components.ego_lanes_network import EgoLanesNetwork
-from Models.model_components.auto_steer_network import AutoSteerNetwork
+from Models.model_components.auto_steer.auto_steer_network import AutoSteerNetwork
 
 
 class AutoSteerNetworkInfer():
     def __init__(self, egolanes_checkpoint_path='', autosteer_checkpoint_path=''):
-
         # Image loader
-        self.image_loader = transforms.Compose(
-            [
-                # transforms.CenterCrop((1440, 2880)),  # e.g. (224, 224),
-                # transforms.Resize((320, 640)),
-                transforms.ToTensor(),
-                transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-            ]
-        )
+        self.image_loader = transforms.Compose([
+            transforms.Lambda(lambda img: img[:, :, ::-1].copy()),  # BGR → RGB
+            transforms.ToTensor(),
+            # transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+        ])
 
         # Checking devices (GPU vs CPU)
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         print(f'Using {self.device} for inference')
 
         # Instantiate model, load to device and set to evaluation mode
-        if (len(egolanes_checkpoint_path) > 0 and len(autosteer_checkpoint_path) > 0):
+        if (len(autosteer_checkpoint_path) > 0):
             # Loading model with full pre-trained weights
-            self.egoLanesNetwork = EgoLanesNetwork()
-            self.egoLanesNetwork.load_state_dict(torch.load \
-                                                     (egolanes_checkpoint_path, weights_only=True,
-                                                      map_location=self.device))
-
-            self.model = AutoSteerNetwork()
-
-            # If the model is also pre-trained then load the pre-trained downstream weights
-            self.model.load_state_dict(torch.load \
-                                           (autosteer_checkpoint_path, weights_only=True, map_location=self.device))
+            self.model = AutoSteerNetwork().load_model(version='n', num_classes=4, checkpoint_path=autosteer_checkpoint_path)
         else:
             raise ValueError('No path to checkpiont file provided in class initialization')
 
-        self.egoLanesNetwork = self.egoLanesNetwork.to(self.device)
-        self.egoLanesNetwork = self.egoLanesNetwork.eval()
         self.model = self.model.to(self.device)
         self.model = self.model.eval()
 
-        # self.feature = torch.zeros_like(torch.randn(1, 64, 10, 20)).to(self.device)
-        self.feature = torch.zeros_like(torch.randn(1, 64, 10, 20)).to(self.device)
-
-        self.image_T_minus_1 = Image.new("RGB", (640, 320), color=(0, 0, 0))
-
+    @torch.no_grad()
     def inference(self, image):
-
-        width, height = image.size
-        if (width != 640 or height != 320):
+        H, W = image.shape[:2]
+        if (W != 1024 or H != 512):
             raise ValueError('Incorrect input size - input image must have height of 320px and width of 640px')
 
-        # self.image_T_mius_1.show()
-        image_tensor_T_minus_1 = self.image_loader(self.image_T_minus_1)
-        image_tensor_T_minus_1 = image_tensor_T_minus_1.unsqueeze(0)
-        image_tensor_T_minus_1 = image_tensor_T_minus_1.to(self.device)
-
-        image_tensor_T = self.image_loader(image)
-        image_tensor_T = image_tensor_T.unsqueeze(0)
-        image_tensor_T = image_tensor_T.to(self.device)
+        image_tensor = self.image_loader(image).unsqueeze(0).to(self.device)
 
         # Run model
-        with torch.no_grad():
-            l1 = self.egoLanesNetwork(image_tensor_T_minus_1)
-            l2 = self.egoLanesNetwork(image_tensor_T)
-            lane_features_concat = torch.cat((l1, l2), dim=1)
-            _, prediction = self.model(lane_features_concat)
-            prediction = prediction.squeeze(0).cpu().detach()
+        xp, h_vector = self.model(image_tensor)
+        xp = xp.detach().cpu().numpy()
+        xp = xp.squeeze()  # (2, 64)
+        h_vector = h_vector.detach().cpu().numpy()
+        h_vector = h_vector.squeeze()  # (2,64)
 
-        # prediction = self.model(image_tensor_T)
-
-        # Get output, find max class probability and convert to steering angle
-        # probs = torch.nn.functional.softmax(prediction, dim=0)
-        output = torch.argmax(prediction).item() - 30
-
-        self.image_T_minus_1 = image.copy()
-
-        return output
+        return  xp, h_vector
